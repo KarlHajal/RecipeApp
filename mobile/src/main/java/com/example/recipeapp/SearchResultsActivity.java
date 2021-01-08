4cfca791b84baffcb23749b99de21377c893c813
karlhajal <karlhajal@gmail.com>
2021-01-08 20:46:05 +0100

    Cleaned useless header

--- mobile/src/main/java/com/example/recipeapp/SearchResultsActivity.java
+++ mobile/src/main/java/com/example/recipeapp/SearchResultsActivity.java
@@ -3,18 +3,9 @@ package com.example.recipeapp;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
