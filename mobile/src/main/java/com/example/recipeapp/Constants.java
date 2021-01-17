a3801afc2968cf8b9ff36674aa04995f0fecad83
karlhajal <karlhajal@gmail.com>
2021-01-17 17:56:26 +0100

    Added another api key in case it's needed

--- mobile/src/main/java/com/example/recipeapp/Constants.java
+++ mobile/src/main/java/com/example/recipeapp/Constants.java
@@ -2,4 +2,5 @@ package com.example.recipeapp;
 
 public class Constants {
     public static final String spoonacularApiKey = "80a1ffa18fa845eaac0f4e8e869392b4";
+    //public static final String spoonacularApiKey = "c1e0505fe0a8496e821d063e1be61d47"; // Another key if needed
 }
