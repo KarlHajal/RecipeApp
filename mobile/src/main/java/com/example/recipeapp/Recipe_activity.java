2231bb41fa38b08522e2cd68542c34cafb91eef3
ankitahumne <ankita.humne@epfl.ch>
2021-01-17 17:10:37 +0100

    changed health score to float

--- mobile/src/main/java/com/example/recipeapp/Recipe_activity.java
+++ mobile/src/main/java/com/example/recipeapp/Recipe_activity.java
@@ -216,7 +216,7 @@ public class Recipe_activity extends AppCompatActivity {
                             title.setText((String) result.getString("title"));
                             ready_in.setText(Integer.toString((Integer) result.get("readyInMinutes")));
                             servings.setText(Integer.toString((Integer) result.get("servings")));
-                            healthy.setText("Health Score:" + (Integer) result.get("healthScore"));
+                            healthy.setText("Health Score: " + result.get("healthScore"));
                             //RecipeID = (int) result.get("id");
                             RecipeAlarmTime = (int) result.get("readyInMinutes");
                             recipe_sourceUrl = (String) result.get("sourceUrl");
