83bfb5f2736ccd547109cc74dbb6f7e83ab3cee0
karlhajal <karlhajal@gmail.com>
2021-01-18 00:16:17 +0100

    Replaced health score with aggregateLikes

--- mobile/src/main/java/com/example/recipeapp/Recipe_activity.java
+++ mobile/src/main/java/com/example/recipeapp/Recipe_activity.java
@@ -326,7 +326,7 @@ public class Recipe_activity extends AppCompatActivity {
                             title.setText((String) result.getString("title"));
                             ready_in.setText(Integer.toString((Integer) result.get("readyInMinutes"))+"mins");
                             servings.setText(Integer.toString((Integer) result.get("servings")));
-                            healthy.setText("Health Score: " + System.lineSeparator() + result.get("healthScore"));
+                            healthy.setText(Integer.toString((Integer) result.get("aggregateLikes")));
                             healthy.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
                             //RecipeID = (int) result.get("id");
                             RecipeAlarmTime = (int) result.get("readyInMinutes");
