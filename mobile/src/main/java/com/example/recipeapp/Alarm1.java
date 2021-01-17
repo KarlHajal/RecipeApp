5ffd52c0c61ec6f8acf874adbf9b4b3989055356
Bar <barbara.tomarchio@hotmail.com>
2021-01-17 18:55:59 +0100

    add timer

--- mobile/src/main/java/com/example/recipeapp/Alarm1.java
+++ mobile/src/main/java/com/example/recipeapp/Alarm1.java
@@ -21,7 +21,7 @@ public class Alarm1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.alarm1);
         timeEt = findViewById(R.id.timeEt);
         startBtn = findViewById(R.id.startBtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
